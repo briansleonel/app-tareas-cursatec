e427995db643598cfa22c3d5c65e479846b17ddc
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-09-17 16:01:32 -0300

    use TareRepository

--- src/main/java/gonzalez/brian/AppTareas/services/TareaService.java
+++ src/main/java/gonzalez/brian/AppTareas/services/TareaService.java
@@ -2,85 +2,78 @@ package gonzalez.brian.AppTareas.services;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import gonzalez.brian.AppTareas.entity.Tarea;
+import gonzalez.brian.AppTareas.repository.TareaRepository;
+import jakarta.transaction.Transactional;
 
+@Service
 public class TareaService {
 
-    private ArrayList<Tarea> tareas;
+	@Autowired
+	private TareaRepository tareaRepository;
 
-    // Constructor
-    public TareaService() {
-        this.tareas = new ArrayList<Tarea>();
-    }
+	// Constructor
+	public TareaService() {
+	}
 
+	// Methods
 
-    // Methods
+	/**
+	 * Esta función permite crear una nueva tarea, a partir de datos provistos y
+	 * guardarlo en la BD
+	 *
+	 * @param titulo      titulo de la tarea
+	 * @param descripcion descripcion de la tarea
+	 * @param fechaLimite fecha límite de la tarea
+	 */
+	@Transactional
+	public void crearTarea(String titulo, String descripcion, LocalDate fechaLimite) throws Exception {
+		// Validando título
+		if (titulo.isEmpty()) {
+			throw new Exception("Debe ingresar un título");
+		} else if (titulo.length() <= 5) {
+			throw new Exception("El título debe tener más de 5 caracteres");
+		}
 
-    /**
-     * Esta función permite crear una nueva tarea, a partir de datos provistos,
-     * y agregarla a la lista de tareas
-     *
-     * @param titulo      titulo de la tarea
-     * @param descripcion descripcion de la tarea
-     * @param fechaLimite fecha límite de la tarea
-     */
-    public void crearTarea(String titulo, String descripcion, LocalDate fechaLimite) throws Exception {
-        // Validando título
-        if (titulo.isEmpty()) {
-            throw new Exception("Debe ingresar un título");
-        } else if (titulo.length() <= 5) {
-            throw new Exception("El título debe tener más de 5 caracteres");
-        }
+		// Validando la descripción
+		if (titulo.isEmpty()) {
+			throw new Exception("Debe ingresar una descripción");
+		}
 
-        // Validando la descripción
-        if (titulo.isEmpty()) {
-            throw new Exception("Debe ingresar una descripción");
-        }
+		Tarea tarea = new Tarea(titulo, descripcion, LocalDate.now(), fechaLimite, true);
+		tareaRepository.save(tarea);
+	}
 
-        Tarea tarea = new Tarea(titulo, descripcion, LocalDate.now(), fechaLimite, true);
-        tareas.add(tarea);
-    }
+	/**
+	 * Permite buscar todas las tareas almacenadas en la BD
+	 * 
+	 * @return
+	 */
+	public List<Tarea> buscarTareas() {
+		return tareaRepository.findAll();
+	}
 
-    /**
-     * Permite buscara una determinada tarea, en el listado de tarea disponibles
-     *
-     * @param titulo titulo de la tarea a buscar
-     * @return
-     */
-    public Tarea buscarTareaPorTitulo(String titulo) {
-        Tarea tareaEncontrada = null;
+	/**
+	 * Permite buscara una determinada tarea por su ID en la BD
+	 *
+	 * @param titulo titulo de la tarea a buscar
+	 * @return
+	 */
+	public Tarea buscarTareaPorID(Integer id) {
+		Tarea tareaEncontrada = tareaRepository.getReferenceById(id);
 
-        for (Tarea tarea : tareas) {
-            if (tarea.getTitulo().equalsIgnoreCase(titulo)) {
-                tareaEncontrada = tarea;
-                break;
-            }
-        }
+		return tareaEncontrada;
+	}
 
-        return tareaEncontrada;
-    }
+	public String formatearFecha(LocalDate fecha) {
+		String formato = "dd/MM/yyyy";
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(formato);
 
-    public String formatearFecha(LocalDate fecha) {
-        String formato = "dd/MM/yyyy";
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(formato);
-
-        return dateFormatter.format(fecha);
-    }
-
-    // Getters and setters
-
-    /**
-     * Mostrar todas las tareas
-     *
-     * @return listado de tares
-     */
-    public ArrayList<Tarea> getTareas() {
-        return tareas;
-    }
-
-    public void setTareas(ArrayList<Tarea> tareas) {
-        this.tareas = tareas;
-    }
+		return dateFormatter.format(fecha);
+	}
 }
